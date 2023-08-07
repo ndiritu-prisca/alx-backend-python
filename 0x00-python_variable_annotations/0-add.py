92ec8c5217e6e0dbe885638b20db7206492b07fc
ndiritu-prisca <priscandiritu@outlook.com>
2023-08-07 23:01:43 +0300

    Basic type annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that demonstrates basic type-annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two float numbers.
+    """
+    return a + b
