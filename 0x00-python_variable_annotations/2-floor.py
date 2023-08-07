bee2b09ebc7510b0aace56e462ac35472732b4da
ndiritu-prisca <priscandiritu@outlook.com>
2023-08-07 23:16:40 +0300

    Return floor of float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module that type-annotates function floor
+"""
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Takes in n as an argument and returns the floor of the float
+    """
+    return math.floor(n)
