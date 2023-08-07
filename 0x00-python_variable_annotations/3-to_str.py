2c0a07c01aef505438ad21ed0e6f42ba7c023561
ndiritu-prisca <priscandiritu@outlook.com>
2023-08-07 23:23:47 +0300

    Type annotate to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that type-annotates function to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Function takes in n as a float argument and returns a string
+        representation of the float
+    """
+    return str(n)
