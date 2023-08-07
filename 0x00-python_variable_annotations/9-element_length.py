519deadc3c898972cb9084fa193cd274d044960b
ndiritu-prisca <priscandiritu@outlook.com>
2023-08-08 00:05:33 +0300

    Documentation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """
-Module that annoatates function element_length
+Module that annotates function element_length
 """
 
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Function that returns List[Tuple[Sequence, int]]
+    """
     return [(i, len(i)) for i in lst]
